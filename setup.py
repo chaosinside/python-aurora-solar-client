aebf790aa3fb680bf8c8316590ec34a7d35e3eff
Chris Hubbard <chubbard@gmail.com>
2020-04-22 19:21:45 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AuroraSolarClient",
-    version="1.0.0",
+    version="1.0.2",
     author="Chris Hubbard",
     author_email="chubbard@gmail.com",
     description="A python client for Aurora Solar",
