eb0344380b7b8b60ff4d72d6ff00f6c0e8302af3
Chris Hubbard <chubbard@gmail.com>
2020-05-07 16:21:58 -0500

    v1.0.4

--- AuroraSolarClient/__init__.py
+++ AuroraSolarClient/__init__.py
@@ -1,1 +1,1 @@
-from client import AuroraSolarClient
+from .client import AuroraSolarClient
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AuroraSolarClient",
-    version="1.0.3",
+    version="1.0.4",
     author="Chris Hubbard",
     author_email="chubbard@gmail.com",
     description="A python client for Aurora Solar",
