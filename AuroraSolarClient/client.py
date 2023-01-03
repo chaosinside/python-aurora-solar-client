0f87dd2744d507a30ed67b740f829d1b2aa21254
Chris Hubbard <chubbard@gmail.com>
2023-01-03 13:38:08 -0600

    added support for API v2022.09

--- AuroraSolarClient/client.py
+++ AuroraSolarClient/client.py
@@ -2,46 +2,89 @@ import requests, json, hashlib, hmac, base64, urllib.parse
 from datetime import datetime
 
 class AuroraSolarClient:
-	# static variables
 	BASE_URL = "https://api.aurorasolar.com"
 	headers = { "Content-Type": "application/json", "Accept": "application/json" }
 
-	def __init__(self, tenant_id, api_key, api_secret):
-		# instance variables
+	def __init__(self, tenant_id, credentials, version="2022.09"):
 		self.tenant_id = tenant_id
-		self.api_key = api_key
-		self.api_secret = api_secret
+		self.version = version
+		if credentials.get("api_token") != None:
+			self.auth_type = "bearer"
+			self.api_token = credentials.get("api_token")
+			self.headers = {**self.headers, "Authorization": "Bearer "+ self.api_token }
+		else:
+			self.auth_type = "hmac"
+			self.api_key = credentials.get("api_key")
+			self.api_secret = credentials.get("api_secret")
 
 	def create_project(self, payload):
-		endpoint = "/v2/tenants/"+ self.tenant_id +"/projects"
-		url = self.signed_url("POST", endpoint, payload)
-		response = requests.post(url, headers=self.headers, data=json.dumps(payload))
+		endpoint = "/tenants/"+ self.tenant_id +"/projects"
+		response = requests.post(self.url(endpoint, "POST", payload), headers=self.headers, data=json.dumps(payload))
 		return response
 
 	def get_project(self, project_id):
-		endpoint = "/v2/tenants/"+ self.tenant_id +"/projects/"+ project_id
-		url = self.signed_url("GET", endpoint)
-		response = requests.get(url, headers=self.headers)
-		return response
+		endpoint = "/tenants/"+ self.tenant_id +"/projects/"+ project_id
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_projects(self):
+		endpoint = "/tenants/"+ self.tenant_id +"/projects"
+		return requests.get(self.url(endpoint), headers=self.headers)
 
 	def get_designs(self, project_id):
-		endpoint = "/v2/tenants/"+ self.tenant_id +"/projects/"+ project_id +"/designs"
-		url = self.signed_url("GET", endpoint)
-		response = requests.get(url, headers=self.headers)
-		return response
+		endpoint = "/tenants/"+ self.tenant_id +"/projects/"+ project_id +"/designs"
+		return requests.get(self.url(endpoint), headers=self.headers)
 
 	def get_design_summary(self, design_id):
-		endpoint = "/v2/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/summary"
-		url = self.signed_url("GET", endpoint)
-		response = requests.get(url, headers=self.headers)
-		return response
+		endpoint = "/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/summary"
+		return requests.get(self.url(endpoint), headers=self.headers)
 
 	def get_design_assets(self, design_id):
-		endpoint = "/v2/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/assets"
-		url = self.signed_url("GET", endpoint)
-		response = requests.get(url, headers=self.headers)
-		return response
+		endpoint = "/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/assets"
+		return requests.get(self.url(endpoint), headers=self.headers)
 
+	def get_design_pricing(self, design_id):
+		endpoint = "/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/pricing"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_design_roof_summary(self, design_id):
+		endpoint = "/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/roof_summary"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_design_web_proposal(self, design_id):
+		endpoint = "/tenants/"+ self.tenant_id +"/designs/"+ design_id +"/web_proposal"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_roles(self):
+		endpoint = "/tenants/"+ self.tenant_id +"/roles"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_sso_provider(self):
+		endpoint = "/tenants/"+ self.tenant_id +"/sso_provider"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_tenant(self):
+		endpoint = "/tenants/"+ self.tenant_id
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_user(self, user_id):
+		endpoint = "/tenants/"+ self.tenant_id +"/users/"+ user_id
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_users(self):
+		endpoint = "/tenants/"+ self.tenant_id +"/users"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	def get_versions(self):
+		endpoint = "/versions"
+		return requests.get(self.url(endpoint), headers=self.headers)
+
+	# returns the proper URL based on version and authentication settings
+	def url(self, endpoint, method="GET", payload=None):
+		if self.version == "2018.01": endpoint = "/v2"+ endpoint
+		if self.auth_type == "hmac": return self.signed_url(method, endpoint, payload)
+		else: return self.BASE_URL + endpoint
+
+	# required for HMAC authentication
 	def signed_url(self, method, endpoint, payload=None):
 		# prepare canonical string
 		key_param = "AuroraKey="+ self.api_key
@@ -54,7 +97,7 @@ class AuroraSolarClient:
 		signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())[:-1]
 		return self.BASE_URL + endpoint +"?"+ key_param +"&"+ timestamp_param +"&Signature="+ signature.decode()
 
-	# payload needs to be sorted by key and flattened for signature
+	# required for HMAC authentication. payload needs to be sorted by key and flattened for signature
 	def order_and_flatten(self, payload):
 		object_name = next(iter(payload))
 		object_data = payload[object_name]
--- README.md
+++ README.md
@@ -2,26 +2,92 @@
 
 A python client for the Aurora Solar API.
 
-### Dependencies
+## Dependencies
 * requests
 
-### Getting Started
+## Getting Started
 
 	pip install AuroraSolarClient
 
-### Usage
-AuroraSolarClient must be initialized with a tenant_id, api_key, and api_secret. Once intialized, you can use any of the provided functions to return a requests response object.
+## Usage
 
-### Example
+### __v2.0.1__
+AuroraSolarClient supports both API versions 2018.01 and v2022.09 as well as HMAC and Bearer token authentication. Instantiation of the client requires `tenant_id` and `credentials`. The third `version` parameter is optional and if not provided, then API v2022.09 is assumed.
+
+	client = AuroraSolarClient(tenant_id, credentials, version)
+
+__tenant_id__ (string)
+: The tenant Id assigned to you by Aurora Solar.
+
+__credentials__ (object)
+: JSON object containting either `api_token` (for bearer authentication), or `api_key` and `api_secret` (for HMAC authentication). If `api_token` exists, the client will automatically use the preferred bearer authentication.
+
+__version__ (string) (optional)
+: The version of AuroraSolar API you wish to use. If not provided, the default is "2022.09". The only other acceptable version is "2018.01".
+
+#### Example using bearer token (most common)
 ```python
 from AuroraSolarClient import AuroraSolarClient
 
 tenant_id = "my_tenant_id"
-api_key = "my_api_key"
-api_secret = "my_api_secret"
-aurora_project_id = "my_aurora_project_id"
+credentials = { "api_token": "sk_prod_1234abc4321cba" }
 
-client = AuroraSolarClient(tenant_id, api_key, api_secret)
-response = client.get_project(aurora_project_id)
+client = AuroraSolarClient(tenant_id, credentials)
+print("Aurora Solar Client", client.version, client.auth_type)
+response = client.get_versions()
 print("response:", response.text)
 ```
+
+#### Example using API v2018.01 with HMAC authentication
+```python
+from AuroraSolarClient import AuroraSolarClient
+
+tenant_id = "my_tenant_id"
+credentials = { 
+	api_key: "my_api_key", 
+	api_secret: "my_api_secret"
+}
+
+client = AuroraSolarClient(tenant_id, credentials, "2018.01")
+print("Aurora Solar Client", client.version, client.auth_type)
+response = client.get_versions()
+print("response:", response.text)
+```
+
+### __v1.0.4__
+AuroraSolarClient only supports API v2018.01, uses HMAC authentication only, and must be initialized with a `tenant_id`, `api_key`, and `api_secret`. Once intialized, you can use any of the provided functions to return a requests response object.
+
+	client = AuroraSolarClient(tenant_id, api_key, api_secret)
+
+__tenant_id__ (string)
+: The tenant Id assigned to you by Aurora Solar.
+
+__api_key__ (string)
+: The API key assigned to you by Aurora Solar.
+
+__api_secret__ (string)
+: The API secret assigned to you by Aurora Solar.
+
+#### Example
+```python
+from AuroraSolarClient import AuroraSolarClient
+
+client = AuroraSolarClient("my_tenant_id", "my_api_key", "my_api_secret")
+response = client.get_project("aurora_project_id")
+print("response:", response.text)
+```
+
+## Change Log
+
+__Note__: v2.0.1 is initialized with different parameters from v1.0.4. __Upgrading from v1.0.4 to v2.0.1 is a breaking change__.
+
+__v2.0.1__
+- Added support for API version 2022.09 while retaining support for 2018.01
+- Added support for bearer authentication while retaining support for HMAC authentication
+- Added more supported endpoints
+
+__v1.0.4__
+- original build out for API version 2018.01
+- pre-official documentation
+- only HMAC authentication supported
+- limited endpoints
